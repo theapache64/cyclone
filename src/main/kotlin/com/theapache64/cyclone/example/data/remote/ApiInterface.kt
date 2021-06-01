fb464cb4b03d122bd45a9b1062968df6b1dc5442
theapache64 <theapache64@gmail.com>
2021-06-02 01:13:33 +0530

    ◀️ backup

--- src/main/kotlin/com/theapache64/cyclone/example/data/remote/ApiInterface.kt
+++ src/main/kotlin/com/theapache64/cyclone/example/data/remote/ApiInterface.kt
@@ -1,14 +1,10 @@
 package com.theapache64.cyclone.example.data.remote
 
 import com.theapache64.cyclone.core.network.Resource
-import com.theapache64.cyclone.example.data.remote.login.LogInRequest
 import com.theapache64.cyclone.example.data.remote.login.LogInResponse
 import com.theapache64.cyclone.example.data.remote.todolist.TodoListResponse
 import kotlinx.coroutines.flow.Flow
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
 
 interface ApiInterface {
 
